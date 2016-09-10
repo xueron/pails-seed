92291053898035f99ed2f47c820c965526fefbdd
倪述荣 <xueron@xueron.com>
2016-09-10 10:32:10 +0800

    add

---
+++ app/Controllers/Admin/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controllers\Admin;
+
+
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+
+    /**
+     * @Route("/admin/index")
+     */
+    public function indexAction()
+    {
+        //echo "hi";
+        //var_dump($this->dispatcher);
+        return;
+    }
+}
+
--- public/index.php
+++ public/index.php
@@ -3,6 +3,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Pails\Application(dirname(__DIR__));
 
-$aaa = new App\Application($app);
-
 $app->run(App\Application::class);
